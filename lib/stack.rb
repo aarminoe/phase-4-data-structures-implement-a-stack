0d01101693e458007ee862eec4716f9c93f8fa58
Aaron Noe <aarminoe@gmail.com>
2022-07-22 19:24:10 -0500

    commit tree 177d007c8782c5aae31b5b726a39cdf92710d984

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,30 @@
 # your code here
+class Stack
+
+    def initialize(limit = 0)
+        @stack = []
+        if limit 
+            @stack = [limit]
+        end
+    end
+
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty 
+        @stack.empty?
+    end
+end
